4b9b68920ce73e0063fc52133ed7d3a60fdcbc6c
GUILLERMO DIAZ <gdiaz@sproutloud.com>
2018-06-29 11:00:32 -0500

    Identation

--- main.js
+++ main.js
@@ -2,75 +2,106 @@
 
 // Main module definition
 const MainController = (function () {
-    
+
     // Constructor
     constructor: {
-        var $users        = $('.users'),
-            $newUserBtn   = $('.new-user'),
+        var $users = $('.users'),
+            $newUserBtn = $('.new-user'),
             $deleteAllBtn = $('.delete-all'),
-            $user         = $('.user');
-        
+            $user = $('.user');
+
         // Add events
         addEvents();
-        
+
         // Clone initial template
         var $userTemplate = $user.clone(true);
     }
-    
+
     // Add init events
     function addEvents() {
         $newUserBtn.click(cloneUserEmpty);
         $deleteAllBtn.click(deleteAll);
         addEventsToUser();
     }
-    
+
     // Add events to user
-    function addEventsToUser(){
+    function addEventsToUser() {
         $user
             .find('.delete').click(deleteUser).end()
             .find('.clone').click(cloneUser);
-        
-            // Set attr selected on change select element
-            $user.find('select').change(function(){
-                $(this)
-                    .find('option[value="'+this.value+'"]')
-                    .attr("selected", "selected");
-            });
+
+        // Set attr selected on change select element
+        $user.find('select').change(function () {
+            $(this)
+                .find('option[value="' + this.value + '"]')
+                .attr("selected", "selected");
+        });
+
+        // Set attr selected on change select element
+        $user.find('input[type="radio"]').click(function () {
+            $(this).attr("checked", "checked");
+            $(this).siblings('input[type="radio"]').removeAttr("checked");
+        });
     }
-    
+
     // Clone user empty
     function cloneUserEmpty() {
         var $newUser = $userTemplate.clone(true).hide();
         $users.append($newUser);
         $newUser.slideDown();
+        reindex();
     }
-    
-     // Delete All Users
+
+    // Delete All Users
     function deleteAll(e) {
         e.preventDefault();
-        
+
         $users.empty();
-    }  
-    
+    }
+
     // Delete User
     function deleteUser(e) {
         e.preventDefault();
-        
+
         var $user = $(e.target).parents('.user');
-        
-        $user.fadeOut( null, null, () => {$user.remove()});
-    }   
-    
+
+        $user.fadeOut(null, null, () => {
+            $user.remove()
+        });
+    }
+
     // Clone User
     function cloneUser(e) {
         e.preventDefault();
         var $userToClone = $(e.target).parents('.user'),
             $newUser = $userToClone.clone(true).hide();
-        
+
         $userToClone.after($newUser);
         $newUser.slideDown();
+        reindex();
     }
-    
+
+    // Rename ids
+    function reindex() {
+        $.each($('.user'), function (i, e) {
+            var $e = $(e),
+                index = $e.index();
+
+            var $elementToReindex = $e.find('[id]');
+
+            $.each($elementToReindex, function (i, e) {
+                var $e = $(e);
+                e.id = $e.data('id') + '-' + index;
+
+                if ($e.next().attr('for')) {
+                    $e.next().attr('for', e.id);
+                }
+            });
+
+
+        });
+    }
+
     // Public data
     return {
         version: 1
